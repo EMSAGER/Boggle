0cdd2bf5568efb4c0eba82103d860327384f5ebc
Emily <emsager7@gmail.com>
2023-12-20 13:00:33 -0800

    initial route push

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 from boggle import Boggle
+from flask import Flask, redirect, render_template, session
+from flask_debugtoolbar import DebugToolbarExtension
 
 boggle_game = Boggle()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '1234Coolio'
+debug = DebugToolbarExtension(app)
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+app.route('/')
+def load_gameboard():
+    """Create boardgame & start session for the game plays"""
+    board = boggle_game.make_board()
+    session['board'] = boggle_game
+    return render_template('home.html', board=board)

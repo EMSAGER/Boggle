845468fc87b44e4648eb8b39d1f126368732794c
Emily <emsager7@gmail.com>
2023-12-20 16:32:01 -0800

    route fix

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from boggle import Boggle
 from flask import Flask, redirect, render_template, session
-from flask_debugtoolbar import DebugToolbarExtension
+#from flask_debugtoolbar import DebugToolbarExtension
+from boggle import Boggle
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1234Coolio'
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-debug = DebugToolbarExtension(app)
+# debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
 
-app.route('/')
+import pdb
+# pdb.set_trace()
+
+@app.route('/')
 def load_gameboard():
     """Create boardgame & start a new session for the game plays"""
     board = boggle_game.make_board()
-    session['board'] = boggle_game
-    return render_template('index.html', board=board)
+    session['board'] = board
+    return render_template("index.html", board=board)

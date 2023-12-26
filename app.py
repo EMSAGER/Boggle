bdbbe3cec85e91839314bca56785ac5751872e72
Emily <emsager7@gmail.com>
2023-12-25 18:04:42 -0800

    final push:

--- app.py
+++ app.py
@@ -11,7 +11,12 @@ def load_gameboard():
     """Create a boardgame and start a new session"""
     board = boggle_game.make_board()
     session['board'] = board
-    return render_template("index.html", board=board)
+
+    #to allow highscore and number of plays to be saved
+    num_plays = session.get("num_plays", 0)
+    highscore = session.get("highscore", 0)
+
+    return render_template("index.html", board=board, highscore=highscore, num_plays=num_plays)
 
 @app.route("/check-word")
 def check_word():
@@ -23,4 +28,18 @@ def check_word():
     #print(f"Checking word: {word}, Result: {response}")
     return jsonify({'result': response})
 
+@app.route("/score", methods=["POST"])
+        #In a Flask application, request.json is used to parse JSON data from the incoming request. When a client sends data to the server using a POST request with the content type set to "application/json," the JSON data is typically included in the request body. The request.json attribute in Flask is a convenient way to access this JSON data.
+def score_plays():
+    """Receive score, update the number of times user played, and update the highscore if needed"""
+    score = request.json["score"]
+    # import pdb
+    # pdb.set_trace()
+    
+    num_plays = session.get("num_plays", 0)
+    highscore = session.get("highscore", 0)
 
+    session['num_plays'] = num_plays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(newRecord= score > highscore)
--- test.py
+++ test.py
@@ -2,14 +2,15 @@ from unittest import TestCase
 from app import app
 from flask import session
 from boggle import Boggle
+import json
 
-app.config['TESTING'] = True
+
 
 class FlaskTests(TestCase):
     def setUp(self):
         """setup before every function - no point in redundancy"""
         self.client = app.test_client()
-    # TODO -- write tests for every view function / feature!
+        app.config['TESTING'] = True
 
     def test_homepage(self):
         """test the homepage's html and session"""
@@ -25,6 +26,8 @@ class FlaskTests(TestCase):
             self.assertIn('board', session)
             self.assertIn(b'Score:', res.data)
             self.assertIn(b'Timer:', res.data)
+            self.assertIsNone(session.get('highscore'))
+            self.assertIsNone(session.get('nplays'))
             
     def test_valid_word(self):
         """testing the check_word path by changing the session template --testing valid word"""
@@ -64,3 +67,31 @@ class FlaskTests(TestCase):
         res = self.client.get('/check-word?word=pasdfasdfsdff')
         self.assertEqual(res.json['result'], 'not-word')
         
+    def test_score(self):
+        """tests the highscore pathway"""
+        with self.client as client:
+            data = {'score': 5}
+                #This line sends a POST request to the '/score' endpoint using the Flask test client (client). It includes the JSON data (data) in the request body, which is achieved by using json.dumps(data) to serialize the dictionary into a JSON-formatted string. The content_type parameter is set to 'application/json' to indicate the type of data being sent.
+            res = client.post('/score', data=json.dumps(data), content_type='application/json')
+                              #After sending the request, this line retrieves the JSON response from the server using the get_json() method. It assumes that the server responds with JSON data, and result now contains the parsed JSON response.
+            result = res.get_json()
+            with client.session_transaction() as sess:
+                highscore = sess.get('highscore')
+            
+            self.assertEqual(res.status_code, 200)
+            self.assertEqual(highscore, 5)
+            self.assertTrue(result['newRecord'])
+
+    def test_num_plays(self):
+        """tests the highscore pathway"""
+        with self.client as client:
+            data = {'score': 5}
+                #This line sends a POST request to the '/score' endpoint using the Flask test client (client). It includes the JSON data (data) in the request body, which is achieved by using json.dumps(data) to serialize the dictionary into a JSON-formatted string. The content_type parameter is set to 'application/json' to indicate the type of data being sent.
+            res = client.post('/score', data=json.dumps(data), content_type='application/json')
+
+            
+            with client.session_transaction() as sess:
+                num_plays = sess.get('num_plays', 1)
+            
+            self.assertEqual(res.status_code, 200)
+            self.assertEqual(num_plays, 1)
